19769b5da8553b4b39d1ed31b3b49649c2dbb42b
zeam2003 <zeam2003@outlook.com>
2021-08-26 14:06:02 -0300

    Clases 1 a 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre = 'Pepe';
+  let auth = true;
+  let estaciones = ["Primavera", "Verano", "Otoño", "Invierno"]
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +12,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>{nombre}</h1>
+        <p>{auth ? "el usuario esta" : "El usuario no esta logueado"}</p>
+        <p>{2 + 1}</p>
+        <ul>
+        {estaciones.map( (el, index) => <li key={index}>{el}</li>)}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
